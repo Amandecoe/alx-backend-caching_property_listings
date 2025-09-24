6c98ca124fd814211211ff0b73ec3f97cd4478ea
Amanuel <amanuelbehailu557@gmail.com>
2025-09-24 13:17:19 +0300

    views made function based and urls created

--- properties/views.py
+++ properties/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 CACHE_TTL = 60 * 15
-class property_list(View):
-    def get(self, request, *args, **kwargs):
+
+@cache_page(CACHE_TTL)
+def property_list(request):
         properties =  Property.Objects.all().values('id','name','price','location')
         data = list(properties)
         return JsonResponse(data, safe=False)
