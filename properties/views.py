0c080ae3bb62ee16117149c4efaca2be520301da
Amanuel <amanuelbehailu557@gmail.com>
2025-09-24 13:13:57 +0300

    views defined

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+CACHE_TTL = 60 * 15
+class property_list(View):
+    def get(self, request, *args, **kwargs):
+        properties =  Property.Objects.all().values('id','name','price','location')
+        data = list(properties)
+        return JsonResponse(data, safe=False)
